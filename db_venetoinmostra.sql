1fb022eba157e8f52530788e4e26714503f8edc2
AlbeChero <albertocherobin@gmail.com>
2018-08-26 10:30:52 +0200

    database ufficiale

--- db_venetoinmostra.sql
+++ db_venetoinmostra.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Ago 26, 2018 alle 11:21
+-- Creato il: Ago 26, 2018 alle 10:29
 -- Versione del server: 10.1.34-MariaDB
--- Versione PHP: 7.2.7
+-- Versione PHP: 7.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `prova`
+-- Database: `db_venetoinmostra`
 --
 
 -- --------------------------------------------------------
@@ -66,32 +66,8 @@ CREATE TABLE `utenti` (
 --
 
 INSERT INTO `utenti` (`id`, `email`, `psw`, `Nome`, `Cognome`) VALUES
-(5, 'alberto.cherobin@gmail.com', 'qqqqqqqq', 'aaaa', 'sss'),
-(6, 'hello@gmail.com', 'alberobrutto', 'Nadia', 'Biasia'),
-(7, 'albechero@gmail.com', 'antonioemarco', 'Albe', 'Chero'),
-(8, 'gg@gmail.com', 'alberobello', 'Antonio', 'Be'),
-(9, 'proviamo@hh.com', 'harrypotter', 'Al', 'zzz'),
-(11, 'aaaaa@jj.com', 'alberobello', 'Albe', 'Maronne'),
-(12, 'hello@gmail.com2', 'alberobello', 'Antonio', 'Be'),
-(13, 'hello@gmail.com3', 'alberobello', 'Antonio', 'sss'),
-(14, 'gg2@gmail.com', 'alberobello', 'Antonio', 'E MARCO'),
-(15, 'ciao12@gmil.com', 'alberobello', 'm arco', 'e luca'),
-(16, 'hello222@gmail.com', 'alberobello', 'maremma', 'maiala'),
-(17, 'gg333@gmail.com', 'alberobello', 'aa', 'sss'),
-(18, 'hel22lo@gmail.com', 'alberobello', 'aa', 'sss'),
-(19, 'alberto.cherobin2@gmail.com', 'alberobello', 'Antonio', 'Be'),
-(20, 'alberto.c33herobin@gmail.com', 'alberobello', 'Antonio', 'sss'),
-(21, 'alber33to.cherobin@gmail.com', 'alberobello', 'Nadia', 'Biasia'),
-(22, 'hellwwwwo@gmail.com', 'alberobello', 'Al', 'sss'),
-(23, 'ciao@gmil.comdddd', 'alberobello', 'aa', 'sss'),
-(24, 'pinguino2000@ggg.com', 'alberobello', 'Antonio', 'Marco'),
-(25, 'ciacccco@gmil.com', 'alberobello', 'Antonio', 'Be'),
-(26, 'ciao@gmil.comsss', 'alberobello', 'Antonio', 'Be'),
-(27, 'hasasdasello@gmail.com', 'alberobello', 'asdasd', 'asdasd'),
-(28, 'ggsss@gmail.com', 'alberobello', 'Al', 'Be'),
-(29, 'hellassaso@gmail.com', 'alberobello', 'asdasd', 'asas'),
-(30, 'ciaasasao@gmil.com', 'alberobello', 'sasasa', 'assaasa'),
-(31, 'helloasas@gmail.com', 'alberobello', 'asassa', 'assasa');
+(1, 'info@venetoinmostra.com', 'venetoinmostra', 'admin', 'del sito'),
+(2, 'albertocherobin@gmail.com', 'alberobello', 'Alberto', 'Cherobin');
 
 -- --------------------------------------------------------
 
@@ -159,7 +135,7 @@ ALTER TABLE `padova`
 -- AUTO_INCREMENT per la tabella `utenti`
 --
 ALTER TABLE `utenti`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT per la tabella `vicenza`
