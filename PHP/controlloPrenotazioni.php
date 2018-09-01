7b6af47a66a5023d4d85175d47359b7272b54c34
Alberto Cherobin <albertocherobin@gmail.com>
2018-09-01 10:37:49 +0100

    sasadaa

--- PHP/controlloPrenotazioni.php
+++ PHP/controlloPrenotazioni.php
@@ -1,7 +1,14 @@
 <?php
         session_start();
 
+        $urlPrec = $_SERVER['HTTP_REFERER'];
+
         if(!isset($_SESSION['username'])){
-            echo
+            $aux = file_get_contents("$urlPrec");
+            echo $aux;
+            echo "Per prenotare devi effettuare laccesso o registrazione";
+        }
+        else{
+
         }
 ?>
