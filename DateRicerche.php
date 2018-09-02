2256facf6cb1990846878adccb67b7dbb8b55b8b
Alberto Cherobin <albertocherobin@gmail.com>
2018-09-02 11:17:28 +0100

    eliminate ridondanze

--- DateRicerche.php
+++ DateRicerche.php
@@ -1,6 +1,5 @@
 <?php
 
-
     session_start();
 
     if(isset($_GET['tipo']))
@@ -20,62 +19,48 @@
           $nav2 = "";
         }
 
+     $page = str_replace('$HEADER$', $nav1, $page);
+     $page= str_replace('$DOWN$', $nav2, $page);
+     $page = str_replace('$CITTA$', $pag, $page);
+     $pag = strtoupper($pag);
+     $page = str_replace('$LUOGO$', $pag, $page);
+     $page= str_replace('$FOOTER$', $footer, $page);
+
+      ob_start();
+
+     if($tipologia == "data"){
+             $_SESSION['DATA'] = $_GET['data'];
+             include "php/date.php";
+        }
+     else {
+             $_SESSION['ricerca'] = $_POST['cerca']; //PRENDO QUELLO CHE E' CERCATO
+             include "php/ricerca.php";
+          }
+
+     $risultati = ob_get_clean();
+
+     $page = str_replace('$PAGINA$', $risultati, $page);
+
+
+
         if (isset($_SESSION['username'])){
                  $username = $_SESSION['username'];
                  $username = strtoupper($username);
-                 $page = str_replace('$HEADER$', $nav1, $page);
+
                  $page = str_replace('$ACCEDI$', "", $page);
                  $page= str_replace('$UTENTE$', $username, $page);
-                 $page= str_replace('$DOWN$', $nav2, $page);
-                 $page= str_replace('$FOOTER$', $footer, $page);
-                 $page = str_replace('$CITTA$', $pag, $page);
-                 $pag = strtoupper($pag);
-                 $page = str_replace('$LUOGO$', $pag, $page);
 
-                 if($username == "ADMIN")
-                 $page = str_replace('$NUOVIARTICOLI$', "NUOVI ARTICOLI", $page);
-                 else $page = str_replace('$NUOVIARTICOLI$', "", $page);
-                 ob_start();
-
-                 if($tipologia == "data"){
-                     $_SESSION['DATA'] = $_GET['data'];
-                     include "php/date.php";
-                 }
-                 else {
-                     $_SESSION['ricerca'] = $_POST['cerca']; //PRENDO QUELLO CHE E' CERCATO
-                     include "php/ricerca.php";
-                 }
-                 $risultati = ob_get_clean();
-
-                 $page = str_replace('$PAGINA$', $risultati, $page);
-                 echo $page;
         }  else {
-                $page = str_replace('$HEADER$', $nav1, $page);
                 $page = str_replace('$ACCEDI$', $bottoniNav1, $page);
                 $page = str_replace('$UTENTE$', "", $page);
-                $page = str_replace('$NUOVIARTICOLI$', "", $page);
-                $page= str_replace('$DOWN$', $nav2, $page);
-                $page = str_replace('$CITTA$', $pag, $page);
-                $page= str_replace('$FOOTER$', $footer, $page);
-                $pag = strtoupper($pag);
-                $page = str_replace('$LUOGO$', $pag, $page);
-
-                ob_start();
-
-                if($tipologia == "data"){
-                     $_SESSION['DATA'] = $_GET['data'];
-                     include "php/date.php";
-                }
-                  else {
-                     $_SESSION['ricerca'] = $_POST['cerca']; //PRENDO QUELLO CHE E' CERCATO
-                     include "php/ricerca.php";
-                 }
-                 $risultati = ob_get_clean();
-
-                 $page = str_replace('$PAGINA$', $risultati, $page);
-                 echo $page;
             }
 
 
+        if(isset($_SESSION['username']) && $username == "ADMIN")
+            $page = str_replace('$NUOVIARTICOLI$', "NUOVI ARTICOLI", $page);
+        else
+            $page = str_replace('$NUOVIARTICOLI$', "", $page);
 
+
+         echo $page;
 ?>
