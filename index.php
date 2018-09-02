506f1c7cc9c5467348de67156cad110f85d15a48
Alberto Cherobin <albertocherobin@gmail.com>
2018-09-02 23:23:49 +0100

    modifiche

--- index.php
+++ index.php
@@ -108,13 +108,12 @@ else{
             $pageHome = str_replace('$FOOTER$', $footer, $pageHome);
             $pageHome = str_replace('$TITOLO$', "Veneto In Mostra | Home", $pageHome);
             if(isset($_SESSION['PAGINA'])) unset($_SESSION['PAGINA']);   //COSI SO QUANDO E' TORNATO ALLA HOME DEL SITO
-            //$_SESSION['pag'] = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
             echo $pageHome;
             exit();
         }
 
 }
 
-        $_SESSION['pag'] = "http://" . $_SERVER['SERVER_NAME'].":8080". $_SERVER['REQUEST_URI'];  //LO USO PER GLI URL DEL SITO
+        $_SESSION['pag'] = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];  //LO USO PER GLI URL DEL SITO
 
 ?>
