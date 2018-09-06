ae66c55e35396c7be870a98ca8e1c84ad1d55359
Alberto Cherobin <albertocherobin@gmail.com>
2018-09-06 15:14:30 +0100

    altra funzione java

---
+++ JS/messaggioPrenotazione.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    alert("Attenzione: si sia assicurato di aver compilato tutti i campi del form sottostante, in caso di mancanza di informazioni, ignoreremo la richiesta. Buona giornata!");
+}
