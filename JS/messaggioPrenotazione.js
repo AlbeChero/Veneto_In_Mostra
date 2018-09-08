c084a440a85136ef07259ac64f278e7036d7c046
Antonio Belculfinè <antoniobcf@hotmail.it>
2018-09-08 11:33:34 +0200

    modifica nomi funzioni

--- JS/messaggioPrenotazione.js
+++ JS/messaggioPrenotazione.js
@@ -1,3 +1,3 @@
-function myFunction() {
+function prenotazione() {
     alert("Attenzione: si sia assicurato di aver compilato tutti i campi del form sottostante, in caso di mancanza di informazioni, ignoreremo la richiesta. Buona giornata!");
 }
