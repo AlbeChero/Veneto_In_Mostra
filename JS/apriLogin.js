91ac8e9e92dc1465104a329e3c15e493afc77b08
Antonio Belculfinè <antoniobcf@hotmail.it>
2018-09-08 19:48:23 +0200

    risolto classe foto home

---
+++ JS/apriLogin.js
@@ -0,0 +1,8 @@
+function apriLogin() {
+    var x = document.getElementById("menuAccedi");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
