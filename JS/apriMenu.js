e7f0b324380d422cf9feeb1201805849130749ae
Antonio Belculfinè <antoniobcf@hotmail.it>
2018-09-08 11:24:05 +0200

    creati file js

---
+++ JS/apriMenu.js
@@ -0,0 +1,8 @@
+function apriMenu() {
+    var x = document.getElementById("menuMobile");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
