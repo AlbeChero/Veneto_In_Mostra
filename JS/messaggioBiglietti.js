9ead4b6c2438bf67776d284822f452ad6c8780b7
Antonio Belculfinè <antoniobcf@hotmail.it>
2018-09-10 13:52:15 +0200

    eliminati file inutili

--- JS/messaggioBiglietti.js
+++
@@ -1,3 +0,0 @@
-function biglietti() {
-    alert("Attenzione: se non hai effettualo l'accesso con profilo al sito non provvederemo ad inviare la tua prenotazione agli organizzatori dell'evento!");
-}
--- JS/messaggioPrenotazione.js
+++
@@ -1,3 +0,0 @@
-function prenotazione() {
-    alert("Attenzione: si sia assicurato di aver compilato tutti i campi del form sottostante, in caso di mancanza di informazioni, ignoreremo la richiesta. Buona giornata!");
-}
